7ecc7073859b58efc03edefb37cc8edfa9520c91
Jonas Gonzalez <alfredojonas94@gmail.com>
2018-10-03 12:54:05 -0400

    Cambios varios para estandarizacion del template

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,7 @@
-import axios from 'axios';
-import VAR   from './env';
+import axios  from 'axios';
+import VAR    from './env';
+import i_logo from '../resources/images/auth/feedback.png';
+
 
 const STATE_VERSION = 0.1;  // Se usa para manejar una version, del estado de la aplicación,
                             // cuando se cambia, setea el storage del cliente por el estado por defecto.
@@ -7,10 +9,13 @@ const MAX_AGE = 18;
 
 axios.defaults.baseURL = VAR.WEB_URL;
 
-const CONST = {
-  ...VAR,
-  STATE_VERSION,
-  MAX_AGE
+const IMG = {
+  logo: i_logo
 };
 
-export default CONST;
+export default {
+  ...VAR,
+  STATE_VERSION,
+  MAX_AGE,
+  IMG
+};
--- src/scenes/auth/components/Footer/index.js
+++ src/scenes/auth/components/Footer/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
 const Footer = props => (
-    <footer
-        style={ {
-          textAlign: 'center'
-        } }
-    >
-      <div style={ { fontFamily: 'Open Sans Cond Light', padding: '10px' } }>
-        {props.t('contactSupport')}
-        <a href="mailto:support@freedom.technology">
-              <span style={ { fontFamily: 'Open Sans Cond Bold' } }>support@freedom.technology</span>
-            </a>
+    <div className="container">
+      <div className="tabs is-centered">
+        <ul>
+          <li><a>Jonas González -- Software developer</a></li>
+        </ul>
       </div>
-    </footer>
+    </div>
 );
 
 export default Footer;
