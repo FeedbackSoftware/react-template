17a505b03abb654f7cbd06470c16ea7ca6da3781
Jose Caceres <josecaceres.oreul@gmail.com>
2018-10-04 10:04:25 -0400

    out folder coverage

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+/*
+ *  Funciones Mocks(simuladas de los modulos de react-native)
+ */
+/*
+ jest.mock('redux', () => ({
+ combineReducers: jest.fn(),
+ applyMiddleware: jest.fn()
+ .mockImplementation((data) => data),
+ requestPermissions: jest.fn(),
+ getFCMToken: jest.fn(() => new Promise((accept, resolve) => accept('FakeToken'))),
+ FCMEvent: {
+ Notification: 'fakeNotification',
+ },
+ }))
+ */
+
+/*
+import configureStore from 'redux-mock-store'
+const middlewares = []
+const mockStore = configureStore(middlewares)
+const initialState = {}
+const store = mockStore(initialState)
+import { Provider }       from 'react-redux';
+
+const { persistor, history } = configureStore(initialState);
+*/
+/*
+ it('renders without crashing', () => {
+ // const fn = jest.fn();
+ const div = document.createElement('div');
+ ReactDOM.render(<Provider store={ store }>
+ <App
+ history={ history }
+ persistor={ persistor }
+ />
+ </ Provider>, div);
+ ReactDOM.unmountComponentAtNode(div);
+ });
+ */
+/*
+describe('Test', () => {
+
+    test('test', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Provider store={ store }>
+            <App
+                history={ history }
+                persistor={ persistor }
+            />
+        </ Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    })
+})
+    */
+
