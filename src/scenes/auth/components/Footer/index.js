2ce14e2ea182a826b675cef136032d04fc912a9b
Jonas Gonzalez <alfredojonas94@gmail.com>
2018-10-04 10:36:55 -0400

    Cambia imagenes, estilos, y templates por defecto

--- src/scenes/auth/components/Footer/index.js
+++ src/scenes/auth/components/Footer/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Footer = props => (
-    <div className="container">
+    <div className="Footer">
+    <footer>
       <div className="tabs is-centered">
         <ul>
           <li><a>Feedback -- Software Enterprise </a></li>
         </ul>
       </div>
+    </footer>
     </div>
 );
 
