d256bbcec194d162ad714cdf30650320a354abcc
Jonas Gonzalez <alfredojonas94@gmail.com>
2018-11-21 12:09:51 -0400

    Agrega pequeño ejemplo de Formiks para el registro, falta agregar un poco de estilo, pero la idea basica es esta

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Loading }     from './Loading';
 export { default as Modal }       from './Modal';
+export { default as FormikExample }       from './FormikExample';
--- src/scenes/auth/signUp/index.js
+++ src/scenes/auth/signUp/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { connect }          from 'react-redux';
-import { Loading }          from '../../../components';
-import { authActions }      from '../../../state/ducks/auth';
-import { NavBar, Footer }   from '../components';
+import React, { Component }       from 'react';
+import { connect }                from 'react-redux';
+import { FormikExample, Loading } from '../../../components';
+import { authActions }            from '../../../state/ducks/auth';
+import { NavBar, Footer }         from '../components';
 import './styles.scss';
-import { ToastContainer }   from 'react-toastify';
-import { TOAST_CONFIG }     from '../../../config/constants';
+import { ToastContainer }         from 'react-toastify';
+import { TOAST_CONFIG }           from '../../../config/constants';
 
 class SignUp extends Component {
   constructor(props) {
@@ -19,66 +19,60 @@ class SignUp extends Component {
 
   componentDidUpdate() {
 
-  };
+  }
 
   render() {
     const { auth: { loading }, showMessage } = this.props;
     return (
-        <div className="SignUp">
-          <ToastContainer />
-          { loading && <Loading /> }
-          <section className="hero is-info is-fullheight">
-            <div className="hero-head">
-              <NavBar />
-            </div>
-            <br />
-            <br />
-            <div className="hero-body">
-              <div className="container has-text-centered">
-                <div className="columns is-vcentered">
-                  <div className="column is-5">
-                    <figure className="image is-4by3">
-                      <img
-                          src="https://picsum.photos/800/600/?random"
-                          alt="Description"
-                      />
-                    </ figure>
-                  </div>
-                  <div className="column is-6 is-offset-1">
-                    <h1 className="title is-2">
+      <div className="SignUp">
+        <ToastContainer />
+        { loading && <Loading /> }
+        <section className="hero is-info is-fullheight">
+          <div className="hero-head">
+            <NavBar />
+          </div>
+          <br />
+          <br />
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <div className="columns is-vcentered">
+                <div className="column is-5">
+                  <FormikExample />
+                </div>
+                <div className="column is-6 is-offset-1">
+                  <h1 className="title is-2">
                       The technological world
-                    </h1>
-                    <h2 className="subtitle is-4">
+                  </h1>
+                  <h2 className="subtitle is-4">
                       A world connected by internet, requires people prepared to
                       do it.
-                    </h2>
-                    <br />
-                    <p className="has-text-centered">
-                      <button
-                          className="button is-medium is-primary is-outlined"
-                          onClick={ () => showMessage('Nothing to show',
-                              TOAST_CONFIG.WARNING) }
-                      >
+                  </h2>
+                  <br />
+                  <p className="has-text-centered">
+                    <button
+                      className="button is-medium is-primary is-outlined"
+                      onClick={ () => showMessage('Nothing to show',
+                          TOAST_CONFIG.WARNING) }
+                    >
                         Learn more
-                      </button>
-                    </p>
-                  </div>
+                    </button>
+                  </p>
                 </div>
               </div>
             </div>
-            <div className="hero-foot">
-              <Footer />
-            </div>
-          </section>
-        </div>
+          </div>
+          <div className="hero-foot">
+            <Footer />
+          </div>
+        </section>
+      </div>
     );
   }
-
 }
 
 const mapStateToProps = ({ auth }) => (
-    {
-      auth,
-    }
+  {
+    auth,
+  }
 );
 export default connect(mapStateToProps, { ...authActions })(SignUp);
