7c6206cb5a45a4a97aac907c6f5557451622c15e
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2019-02-20 09:38:11 -0400

    Updates packages for i18next

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,40 +1,40 @@
-import i18n                   from 'i18next';
-import LanguageDetector       from 'i18next-browser-languagedetector';
-import { reactI18nextModule } from 'react-i18next';
-import { en, es }             from './languages';
-import { authActions }        from '../state/ducks/auth';
+import i18n                 from 'i18next';
+import LanguageDetector     from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import { en, es }           from './languages';
+import { authActions }      from '../state/ducks/auth';
 
 const configurei18n = (store) => {
   i18n
-      .use(LanguageDetector)
-      .use(reactI18nextModule)
-      .on('languageChanged', (lng) => {
-        store.subscribe(() => {
-          const { auth } = store.getState();
-          const language = lng.split('-')[0];
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .on('languageChanged', (lng) => {
+      store.subscribe(() => {
+        const { auth } = store.getState();
+        const language = lng.split('-')[0];
 
-          if (auth.logged && language !== auth.language) {
-            store.dispatch(authActions.setLanguage({ language }));
-          }
-        });
-      })
-      .init({
-        fallbackLng: 'en',
-        // have a common namespace used around the full app
-        ns: ['translations'],
-        defaultNS: 'translations',
-        debug: true,
-        interpolation: {
-          escapeValue: false, // not needed for react!!
-        },
-        react: {
-          wait: true,
-        },
-        resources: {
-          en,
-          es,
-        },
+        if (auth.logged && language !== auth.language) {
+          store.dispatch(authActions.setLanguage({ language }));
+        }
       });
+    })
+    .init({
+      fallbackLng: 'en',
+      // have a common namespace used around the full app
+      ns: ['translations'],
+      defaultNS: 'translations',
+      debug: true,
+      interpolation: {
+        escapeValue: false, // not needed for react!!
+      },
+      react: {
+        wait: true,
+      },
+      resources: {
+        en,
+        es,
+      },
+    });
 };
 
 export default configurei18n;
